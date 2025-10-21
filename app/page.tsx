472e3e980e75841065d7a13d0ad07bcd1ef138d5
umair-hussain02 <ua353000@gmail.com>
2025-10-22 02:47:33 +0500

    refactor: comment out SuccessStoriesSection in HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function HomePage() {
           <HowItWorksSection />
           <PartnersSection />
           <WhyChooseSection />
-          <SuccessStoriesSection />
+          {/* <SuccessStoriesSection /> */}
           <BlogSection />
           <CTASection />
         </main>
